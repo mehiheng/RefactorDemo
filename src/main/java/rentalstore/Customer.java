b8ec89ddef03d5b8c5323d262229a963e1ace6dc
ZHOUAD <Jing690518>
2018-08-24 22:16:19 +0800

    add NEW_RELEASE test

--- src/main/java/rentalstore/Customer.java
+++ src/main/java/rentalstore/Customer.java
@@ -6,7 +6,8 @@ import java.util.Vector;
 public class Customer {
     private String name;
     private Vector rentals = new Vector();
-
+    double totalAmount = 0;
+    int frequentRenterPoints = 0;
     public Customer(String name) {
         this.name = name;
     }
@@ -20,14 +21,9 @@ public class Customer {
     }
 
     public String statement(){
-        double totalAmount = 0;
-        int frequentRenterPoints = 0;
         Enumeration rentals = this.rentals.elements();
         String result = "<H1>Rental Record for <EM>" + getName() + "</EM></H1><P>\n";
-        PrintBodyResult printBodyResult = new PrintBodyResult(totalAmount, frequentRenterPoints, rentals, result).invoke();
-        totalAmount = printBodyResult.getTotalAmount();
-        frequentRenterPoints = printBodyResult.getFrequentRenterPoints();
-        result = printBodyResult.getResult();
+        result += printBodyResult(rentals, result);
 
         //add footer lines
         result += "<P>Amount owed is<EM>" + String.valueOf(totalAmount) + "</EM><P>\n";
@@ -35,66 +31,40 @@ public class Customer {
         return result;
     }
 
-    private class PrintBodyResult {
-        private double totalAmount;
-        private int frequentRenterPoints;
-        private Enumeration rentals;
-        private String result;
+    private String printBodyResult(Enumeration rentals, String result) {
+        while(rentals.hasMoreElements()){
+            double thisAmount =0;
+            Rental each = (Rental) rentals.nextElement();
 
-        public PrintBodyResult(double totalAmount, int frequentRenterPoints, Enumeration rentals, String result) {
-            this.totalAmount = totalAmount;
-            this.frequentRenterPoints = frequentRenterPoints;
-            this.rentals = rentals;
-            this.result = result;
-        }
-
-        public double getTotalAmount() {
-            return totalAmount;
-        }
-
-        public int getFrequentRenterPoints() {
-            return frequentRenterPoints;
-        }
-
-        public String getResult() {
-            return result;
-        }
-
-        public PrintBodyResult invoke() {
-            while(rentals.hasMoreElements()){
-                double thisAmount =0;
-                Rental each = (Rental) rentals.nextElement();
-
-                switch (each.getMovie().getPriceCode()){
-                    case Movie.REGULAR:
-                        thisAmount += 2;
-                        if(each.getDayRented() > 2){
-                            thisAmount+=(each.getDayRented() - 2) * 1.5;
+            switch (each.getMovie().getPriceCode()){
+                case Movie.REGULAR:
+                    thisAmount += 2;
+                    if(each.getDayRented() > 2){
+                        thisAmount+=(each.getDayRented() - 2) * 1.5;
+                    }
+                    break;
+                case Movie.NEW_RELEASE:
+                    thisAmount+=each.getDayRented()*3;
+                    break;
+                    case Movie.CHILDRENS:
+                        thisAmount+=1.5;
+                        if(each.getDayRented() > 3){
+                            thisAmount += (each.getDayRented() -3)*1.5;
                         }
                         break;
-                    case Movie.NEW_RELEASE:
-                        thisAmount+=each.getDayRented()*3;
-                        break;
-                        case Movie.CHILDRENS:
-                            thisAmount+=1.5;
-                            if(each.getDayRented() > 3){
-                                thisAmount += (each.getDayRented() -3)*1.5;
-                            }
-                            break;
-                }
-
-                //add frequent renter points
-                frequentRenterPoints ++;
-                //add bonus for a two day new release rental
-                if((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDayRented() > 1){
-                    frequentRenterPoints ++;
-                }
-
-                //show figures for this rental
-                result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "<BR>\n";
-                totalAmount += thisAmount;
             }
-            return this;
+
+            //add frequent renter points
+            frequentRenterPoints ++;
+            //add bonus for a two day new release rental
+            if((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDayRented() > 1){
+                frequentRenterPoints ++;
+            }
+
+            //show figures for this rental
+            result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "<BR>\n";
+            totalAmount += thisAmount;
         }
+        return result;
     }
 }
--- src/test/java/rental_store/rentalTest.java
+++ src/test/java/rental_store/rentalTest.java
@@ -37,7 +37,7 @@ public class rentalTest {
     }
 
     @Test
-    public void when_rent_CHILDRENS_3_days_REGULAR_3days_return_amount_5() throws Exception {
+    public void when_rent_CHILDRENS_3_days_REGULAR_3_days_return_amount_5_and_frequentRenterPoints_2() throws Exception {
         Customer customer=new Customer("renter");
         Movie movie=new Movie("TomAndJerry",Movie.CHILDRENS);
         Movie movie1=new Movie("kingOfRing",Movie.REGULAR);
@@ -53,6 +53,25 @@ public class rentalTest {
         String frequentRenterPoints=rentOrder.substring(indexOfEarned+10,indexOfFrequent);
         assertThat(String.valueOf((double) 5), is(totalAmount));
         assertThat(String.valueOf(2), is(frequentRenterPoints));
+    }
+
+    @Test
+    public void when_rent_CHILDRENS_3_days_NEW_RELEASE_1_days_return_amount_ten_point_five_and_frequentRenterPoints_1() throws Exception {
+        Customer customer=new Customer("renter");
+        Movie movie=new Movie("TomAndJerry",Movie.CHILDRENS);
+        Movie movie1=new Movie("AntMan",Movie.NEW_RELEASE);
+        Rental rental=new Rental(movie,3);
+        Rental rental1=new Rental(movie1,3);
+        customer.addRental(rental);
+        customer.addRental(rental1);
+        String rentOrder=customer.statement();
+        int indexOfis=rentOrder.indexOf("is<EM>");
+        int indexOfEarned=rentOrder.indexOf("earned<EM>");
+        int indexOfFrequent=rentOrder.indexOf(" </EM>frequent");
+        String totalAmount=rentOrder.substring(indexOfis+6,indexOfis+10);
+        String frequentRenterPoints=rentOrder.substring(indexOfEarned+10,indexOfFrequent);
+        assertThat(String.valueOf(10.5), is(totalAmount));
+        assertThat(String.valueOf(3), is(frequentRenterPoints));
     }
 
 }
