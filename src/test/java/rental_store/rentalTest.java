43e5d810f0c04635ee6ae511272c2f815cdc650f
ZHOUAD <Jing690518>
2018-08-24 14:40:56 +0800

    totalAmout and frequentRenterPoints test

--- src/test/java/rental_store/rentalTest.java
+++ src/test/java/rental_store/rentalTest.java
@@ -38,15 +38,25 @@ public class rentalTest {
         assertThat(String.valueOf(1.5), is(totalAmount));
     }
 
-//    @Test
-//    public void should_return_200_when_payAmount_given_employee_is_Salesman_and_monthly_salary_is_100_and_commission_is_100() throws Exception {
-//        Employee salesman = new Employee(1);
-//        salesman.setMonthlySalary(100);
-//        salesman.setCommission(100);
-//
-//        int payAmount = salesman.payAmount();
-//
-//        assertEquals(200, payAmount);
-//    }
+    @Test
+    public void when_rent_CHILDRENS_3_days_REGULAR_3days_return_amount_5() throws Exception {
+        Customer customer=new Customer("renter");
+        Movie movie=new Movie("TomAndJerry",Movie.CHILDRENS);
+        Movie movie1=new Movie("kingOfRing",Movie.REGULAR);
+        Rental rental=new Rental(movie,3);
+        Rental rental1=new Rental(movie1,3);
+        customer.addRental(rental);
+        customer.addRental(rental1);
+        String rentOrder=customer.statement();
+        int a=rentOrder.indexOf("is");
+        int b=rentOrder.indexOf("You");
+        int c=rentOrder.indexOf("earned");
+        int d=rentOrder.indexOf(" frequent");
+        String totalAmount=rentOrder.substring(a+2,a+5);
+        String frequentRenterPoints=rentOrder.substring(c+6,d);
+        assertThat(String.valueOf((double) 5), is(totalAmount));
+        assertThat(String.valueOf(2), is(frequentRenterPoints));
+    }
+
 }
 
