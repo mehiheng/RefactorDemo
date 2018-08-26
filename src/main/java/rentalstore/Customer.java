f3c2aab3b85d1d5a4c2159b6c25e3f5e24324ec4
ZHOUAD <Jing690518>
2018-08-26 15:03:57 +0800

    change switch to if

--- src/main/java/rentalstore/Customer.java
+++ src/main/java/rentalstore/Customer.java
@@ -33,23 +33,20 @@ public class Customer {
         while(rentals.hasMoreElements()){
             double thisAmount =0;
             Rental each = (Rental) rentals.nextElement();
-
-            switch (each.getMovie().getPriceCode()){
-                case Movie.REGULAR:
-                    thisAmount += 2;
-                    if(each.getDayRented() > 2){
-                        thisAmount+=(each.getDayRented() - 2) * 1.5;
-                    }
-                    break;
-                case Movie.NEW_RELEASE:
-                    thisAmount+=each.getDayRented()*3;
-                    break;
-                    case Movie.CHILDRENS:
-                        thisAmount+=1.5;
-                        if(each.getDayRented() > 3){
-                            thisAmount += (each.getDayRented() -3)*1.5;
-                        }
-                        break;
+            if(each.getMovie().getPriceCode()==Movie.REGULAR){
+                thisAmount += 2;
+                if(each.getDayRented() > 2){
+                    thisAmount+=(each.getDayRented() - 2) * 1.5;
+                }
+            }
+            if(each.getMovie().getPriceCode()==Movie.NEW_RELEASE){
+                thisAmount+=each.getDayRented()*3;
+            }
+            if(each.getMovie().getPriceCode()==Movie.CHILDRENS){
+                thisAmount+=1.5;
+                if(each.getDayRented() > 3){
+                    thisAmount += (each.getDayRented() -3)*1.5;
+                }
             }
             frequentRenterPoints(each);
             result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "<BR>\n";
